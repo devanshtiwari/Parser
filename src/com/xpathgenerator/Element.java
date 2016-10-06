89c4691ef704ba6a54ed8a363b529dd11c0b8a8b
Devansh Tiwari <devanshtiwari@iiitdmj.ac.in>
2016-10-06 16:10:05 +0530

    Added Comments and Modifications in Variable Name

--- src/com/xpathgenerator/Element.java
+++ src/com/xpathgenerator/Element.java
@@ -4,13 +4,17 @@ import java.util.HashMap;
 import java.util.Objects;
 
 /**
- * Created by avinaana on 10/4/2016.
+ * Created by Devansh and Avinash on 10/4/2016.
  */
 
+/**
+ *
+ */
 public class Element {
     private String name = "", parent = "", grandParent = "", xpath ="";
-    private HashMap<String,String> attrs1 = new HashMap<>();
-    private HashMap<String, String> attrs2 = new HashMap<>();
+    private HashMap<String,String> attrsOR = new HashMap<>();
+    private HashMap<String, String> attrsAND = new HashMap<>();
+
     public Element() {
     }
     public Element(String name){
@@ -25,37 +29,57 @@ public class Element {
         this.parent = parent;
         this.grandParent = grandParent;
     }
-    public Element(String name, String parent, String grandParent, HashMap<String,String> attrs1){
+    public Element(String name, String parent, String grandParent, HashMap<String,String> attrs){
         this.name = name;
         this.parent = parent;
         this.grandParent = grandParent;
-        this.attrs1 = attrs1;
+        this.attrsOR = attrs;
     }
     public Element(String name, String parent, String grandParent, HashMap<String,String> attrs1, HashMap<String, String> attrs2){
         this.name = name;
         this.parent = parent;
         this.grandParent = grandParent;
-        this.attrs1 = attrs1;
-        this.attrs2=attrs2;
+        this.attrsOR = attrs1;
+        this.attrsAND=attrs2;
     }
 
+    /**
+     *
+     * @param name name of the attribute node
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @param parent Parent of the attribute node to be searched for
+     */
     public void setParent(String parent) {
         this.parent = parent;
     }
 
+    /**
+     *
+     * @param grandParent Grandparent the the Element Node
+     */
     public void setGrandParent(String grandParent) {
         this.grandParent = grandParent;
     }
 
-    public void setAttrs1(HashMap<String,String> attrs1) {
-        this.attrs1 = attrs1;
+    /**
+     *
+     * @param attrs Addition of Attribute conditions with OR Condition as Hashmap
+     */
+    public void setAttrsOR(HashMap<String,String> attrs) {
+        this.attrsOR = attrs;
     }
 
-    public void setAttrs2(HashMap<String,String> attrs2) { this.attrs2 = attrs2; }
+    /**
+     *
+     * @param attrs Addition of Attribute conditions with AND Condition as Hashmap
+     */
+    public void setAttrsAND(HashMap<String,String> attrs) { this.attrsAND = attrs; }
 
     public void addAttribute(String key,int flag){
         addAttribute(key,"",flag);
@@ -63,16 +87,21 @@ public class Element {
 
     /**
      *
-     * @param key
-     * @param value
+     * @param key Key for the new attribute
+     * @param value Value related to the attribute
      * @param flag IF 0 then OR otherwise AND
      */
     public void addAttribute(String key, String value,int flag){
         if(flag==0)
-            this.attrs1.put(key, value);
+            this.attrsOR.put(key, value);
         else
-            this.attrs2.put(key,value);
+            this.attrsAND.put(key,value);
     }
+
+    /**
+     *
+     * @return Returns the XPath on the given constraints.
+     */
 
     public String getXpath(){
         xpath += "//";
@@ -82,39 +111,43 @@ public class Element {
         addSlash();
         xpath += this.name;
         System.out.println(xpath);
-        if(!attrs1.isEmpty() || !attrs2.isEmpty())
+        if(!attrsOR.isEmpty() || !attrsAND.isEmpty())
             xpath +="[ ";
-        for(String key: attrs1.keySet()){
-            if(!attrs1.get(key).equals(""))
-                xpath += ("@"+key+"='"+ attrs1.get(key))+"'";
+        for(String key: attrsOR.keySet()){
+            if(!attrsOR.get(key).equals(""))
+                xpath += ("@"+key+"='"+ attrsOR.get(key))+"'";
             else
                 xpath += ("@"+key);
             xpath += " or ";
         }
         if(xpath.endsWith("or "))
             xpath=xpath.substring(0,xpath.length()-3);
-        if(attrs2.isEmpty())
+        if(attrsAND.isEmpty())
             xpath+="]";
         else
             xpath+="and ";
         System.out.println(xpath);
-        for(String key:attrs2.keySet())
+        for(String key:attrsAND.keySet())
         {
-            if(!attrs2.get(key).equals(""))
-                xpath += ("@"+key+"='"+ attrs2.get(key)+"'");
+            if(!attrsAND.get(key).equals(""))
+                xpath += ("@"+key+"='"+ attrsAND.get(key)+"'");
             else
                 xpath += ('@'+key);
             xpath += " and ";
         }
         if(xpath.endsWith("and "))
             xpath=xpath.substring(0,xpath.length()-4);
-        if(!attrs2.isEmpty())
+        if(!attrsAND.isEmpty())
             xpath+="]";
         addSlash();
         if(xpath.substring(xpath.length()-1).equals("/"))
             xpath=xpath.substring(0,xpath.length()-1);
        return xpath;
     }
+
+    /**
+     * Utility Function to add Slash for Xpath.
+     */
     private void addSlash(){
         String slash = "/";
         String lastChar = xpath.substring(xpath.length()-1);
